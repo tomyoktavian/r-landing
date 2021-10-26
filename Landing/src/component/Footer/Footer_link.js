f5080af32f4f227f5bfcf262baa29bae2481a606
cloudmicrosystem <cloudmicrosystem@gmail.com>
2021-10-27 01:44:25 +0700

    first commit

---
+++ Landing/src/component/Footer/Footer_link.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Row, Col } from 'reactstrap';
+
+class Footer_link extends Component {
+  render() {
+    return (
+      // Footer Link start
+      <Row>
+        <Col lg={12}>
+          <div className="text-center">
+            <p className="text-muted mb-0">{(new Date().getFullYear())}{" "} © Thamza. Develop by Themesdesign</p>
+          </div>
+        </Col>
+      </Row>
+      //   Footer Link End
+    );
+  }
+}
+
+export default Footer_link;
